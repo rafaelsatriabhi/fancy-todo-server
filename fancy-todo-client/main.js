c78e6171b701b7ced48132c77120452ba9aaacf4
Rafael Atheya <rafael.satriabhi@gmail.com>
2020-09-05 17:56:39 +0700

    Google Calendar masih dalam tahap percobaan, Sign-in lewat google masih error

---
+++ fancy-todo-client/main.js
@@ -0,0 +1,189 @@
+const baseUrl = `http://localhost:3000`
+
+$(document).ready(()=>{
+    checkAuth()
+})
+
+function login(event){
+   event.preventDefault()
+   
+    let email = $(`#email-input`).val()
+    let password = $(`#password-input`).val()
+
+    $.ajax(`${baseUrl}/users/login`, {
+        method:`POST`,
+        data:{
+            email,
+            password
+        }
+    })
+    .done((data)=>{
+        localStorage.token = data.token
+        checkAuth()
+    })
+    .fail((err)=>{
+        console.log(err)
+    })
+}
+function checkAuth(){
+    $(`#login-page`).show()
+    $(`#homepage`).hide()
+    if(localStorage.token){
+        $(`#login-page`).hide()
+        $(`#homepage`).show()
+        $(`#createTodo`).hide()
+        $(`#updateTodo`).hide()
+
+
+        fetchTodos()
+        console.log("Sudah login")
+    }
+    else{
+        $(`#login-page`).show()
+        $(`#homepage`).hide()
+        $(`#createTodo`).hide()
+        $(`#updateTodo`).hide()
+        console.log("Belum login")
+    }
+}
+
+function logout(){
+    localStorage.clear()
+    checkAuth()
+}
+
+function fetchTodos(){
+    $.ajax(`${baseUrl}/todos`,{
+        method:"GET",
+        headers:{
+            token: localStorage.token
+        }
+    })
+    .done((data)=>{
+        $(`#todosTable`).empty()
+        let column = `<tr>
+        <th>No</th>
+        <th>Title</th>
+        <th>Descrption</th>
+        <th>Due date</th>
+        <th>Action</th>
+
+    </tr>`
+        $(`#todosTable`).append(column)
+
+       data.forEach((element,idx)=>{
+        let template = `<tr>
+        <td>${idx+1}</td>
+        <td>${element.title}</td>
+        <td>${element.description}</td>
+        <td>${element.due_date}</td>
+        <td>
+        <button onclick="updatePage(${element.id})">Update</button> ||
+        <button onclick="deleteTodo(${element.id})">Done</button>
+        </td>
+
+    </tr>`
+        $(`#todosTable`).append(template)
+       })
+    })
+    .fail((err)=>{
+        console.log(err)
+    })
+}
+
+function addTodoPage(){
+    $(`#homepage`).hide()
+    $(`#createTodo`).show()
+}
+
+function createTodo(){
+
+    let title = $(`#title-input`).val()
+    let description = $(`#description-input`).val()
+    let due_date = $(`#dueDate-input`).val()
+    let status = $(`#status-input`).val()
+
+
+    $.ajax(`${baseUrl}/todos`,{
+        method: "POST",
+        data:{
+            title,
+            description,
+            due_date,
+            status,
+
+        }
+    })
+    .done((data)=>{
+        console.log(data)
+    })
+    .fail((err)=>{
+        console.log(err)
+    })
+}
+
+function deleteTodo (params){
+  $.ajax(`${baseUrl}/todos/${params}`,{
+      method:"DELETE"
+  })
+  .done((data)=>{
+      console.log(data)
+  })
+}
+
+
+function updatePage(params){
+    $(`#homepage`).hide()
+    $(`#updateTodo`).show()
+    localStorage.id = params
+}
+
+function updateTodo(event){
+   event.preventDefault()
+    let title = $(`#title-update`).val()
+    let description = $(`#description-update`).val()
+    let due_date = $(`#dueDate-update`).val()
+    let status = $(`#status-update`).val()
+
+
+    $.ajax(`${baseUrl}/todos/${localStorage.id}`,{
+        method: "PUT",
+        data:{
+            title,
+            description,
+            due_date,
+            status,
+        }
+    })
+    .done((data)=>{
+        console.log(data)
+    })
+    .fail((err)=>{
+        console.log(err)
+    })
+}
+
+function onSignIn(googleUser) {
+    let profile = googleUser.getBasicProfile()
+    console.log("ID: " + profile.getId())
+    console.log('Full Name: ' + profile.getName())
+    console.log('Given Name: ' + profile.getGivenName())
+    console.log('Family Name: ' + profile.getFamilyName())
+    console.log("Image URL: " + profile.getImageUrl())
+    console.log("Email: " + profile.getEmail())
+  
+    let id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token)
+    $.ajax({
+    method: 'POST',
+    url: `http://localhost:3000/user/tokensignin`,
+    data: { id_token: id_token }
+    })
+    .done((response)=>{
+      localStorage.setItem('token',response.token)
+      window.location.href = "dashboard.html"
+    })
+    .fail((jqXHR, textStatus)=>{
+      console.log(textStatus)
+    })
+  }
--- fancy-todo-server/controllers/controllers.js
+++ fancy-todo-server/controllers/controllers.js
@@ -1,74 +1,7 @@
 const {Todo,User} = require(`../models/index.js`)
 const bcrypt = require(`bcryptjs`)
 const jwt = require(`jsonwebtoken`)
-const {google} = require(`googleapis`)
-const {oAuth2} = google.auth
 
-const oAuth2Client = new OAuth2(`413161148313-ned615lolvcm7l4lbrv305t9ls9ssc1k.apps.googleusercontent.com`,`BObxFuIGd6rmSHtAccORNyPB`)
-
-
-oAuth2Client.setCredentials({
-    refresh_token:"1//04wnAoYCFYFuACgYIARAAGAQSNwF-L9Ir9a2X6EIpNPyjto4XgQgNkTtf0TjOLi4_CpIdbinEE4sMQvDmxp5Et_Sbo1iLDu2pv_o"
-})
-
-
-const calendar = google.calendar({
-    version:"v3",
-    auth: oAuth2Client
-})
-
-//Ini input untuk ngetes aja apa GCalendarnya bisa atau enggak, seharusnya input ini didapat dari Body form
-const eventStarTime = new Date ()
-
-eventStarTime.setDate(eventStarTime.getDay() + 2)
-
-const eventEndTime = new Date ()
-eventEndTime.setDate(eventEndTime.getDate + 2)
-eventEndTime.setMinutes(eventEndTime.getMinutes() + 45)
-
-const event = {
-    title: "Makan-makan bareng temen kantur",
-    description: "Acara makan-makan bersama owner, YEYY!!",
-    start:{
-        dateTime: eventStarTime,
-        timeZone: "Indonesia/Jakarta"
-    },
-    end:{
-        dateTime: eventEndTime,
-        timeZone: "Indonesia/Jakarta"
-    },
-    colorId:8,
-}
-
-calendar.freebusy.query({
-    resource:{
-        timeMin: eventStarTime,
-        timeMax:eventEndTime,
-        timeZone: "Indonesia/Jakarta",
-        items: [{id:"primary"}],
-    }
-},
-    (err,res)=>{
-        if(err){
-            console.error(`Free Busy Query Error: ${err}`)
-        }
-        else{
-            const eventsArr = res.data.calendars.primary.busy
-        }
-        if(eventsArr.length === 0){
-            calendar.events.insert({calendarId:"primary",resource: event},
-            err =>{
-                if(err){
-                    console.error(`Calendar Event Creation Error: ${err}`)
-                }
-                else{
-                    console.log(`Calendar Event Created`)
-                }
-            })
-            console.log("Jadwal penuh di hari tersebut")
-        }
-    }
-)
 
 class Controller{
     static postTodos(req,res){
@@ -282,6 +215,42 @@ class Controller{
             res.send(err)
         })
     }
+    static googleLoginHandler(req, res, next) {
+        let payload;
+        gClient.verifyIdToken({
+          idToken: req.body.id_token,
+          audience: process.env.google_oauth_clientId
+        })
+          .then((ticket) => {
+            payload = ticket.getPayload();
+            return User.findOne({ where: { email: payload.email } })
+          })
+          .then((data) => {
+            if (data === null) {
+              return User.create({
+                name: payload.name,
+                email: payload.email,
+                password: `${payload.name}`
+              })
+            } else {
+              return data;
+            }
+          })
+          .then(() => {
+            let frontendPayload = {
+              name: payload.name,
+              email: payload.email
+            }
+            let token = jwt.sign(frontendPayload, process.env.JWT_SECRET_KEY)
+            res.status(200).json({ token })
+          })
+          .catch(() => {
+            res.status(400).json({
+              message: "Invalid email/password"
+            })
+          })
+    
+      }
 }
 
 module.exports = Controller
