0a1a39d840e3e16cde5ac5375ff512c94e294ec7
Rafael Atheya <rafael.satriabhi@gmail.com>
2020-09-25 11:11:26 +0700

    Authentication, tambah code untuk cek apakah usernya masih ada di database atau engggak

--- fancy-todo-server/middlewares/authentication.js
+++ fancy-todo-server/middlewares/authentication.js
@@ -1,11 +1,27 @@
 const jwt = require('jsonwebtoken')
 
+const { User } = require('../models')
+
 function authentication (req, res, next) {
   if(req.headers.token) {
     try{
       const payload = jwt.verify(req.headers.token, process.env.JWT_SECRET_KEY)
-      req.loggedInUser = payload
-      next()
+
+      User.findOne({ //cek apakah usernya masih ada atau enggak di database
+        where: {
+          id: payload.id
+        }
+      })
+      .then(data=>{
+        if(data){
+          req.loggedInUser = payload
+          next()
+        } else {
+          res.status(401).json({
+            message: "Failed to authenticate"
+          })
+        }
+      })
     }
     catch(err) {
       res.status(401).json({
